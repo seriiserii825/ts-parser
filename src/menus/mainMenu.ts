c85d77a22ef121921906b949d7a81f656fa91d82
serii <seriiburduja@gmail.com>
2025-10-04 18:36:29 +0300

    feat: mainMenu foreach finished

--- src/menus/mainMenu.ts
+++ src/menus/mainMenu.ts
@@ -3,6 +3,7 @@ import HtmlParse from "../classes/HtmlParse.js";
 import seoMenu from "./seoMenu.js";
 import imageMenu from "./imageMenu.js";
 import htmlMenu from "./htmlMenu.js";
+import chalk from "chalk";
 
 type Section = "seo" | "images" | "links" | "exit";
 
@@ -25,7 +26,7 @@ export default async function mainMenu(urls: string[]): Promise<void> {
       seo: [] as string[],
       images: [] as string[],
       links: [] as string[],
-    }
+    };
 
     if (section === "seo") {
       menu_choices.seo = await htmlMenu([
@@ -39,10 +40,22 @@ export default async function mainMenu(urls: string[]): Promise<void> {
       ]);
     }
 
+    if (section === "images") {
+      menu_choices.images = await htmlMenu([
+        { label: "All", value: "all" },
+        { label: "With alt", value: "alt" },
+        { label: "Without alt", value: "no-alt" },
+        { label: 'With loading="lazy"', value: "lazy" },
+        { label: "Without loading attribute", value: "no-loading" },
+        { label: "Back", value: "back" },
+        { label: "Exit", value: "exit" },
+      ]); 
+    }
+
     // прогоняем выбранное действие по всем URL
     for (const url of urls) {
       const parser = new HtmlParse(url);
-      console.log(`\n— Processing: ${url}`);
+      console.log(chalk.yellow(`\n— Processing: ${url}`));
 
       if (section === "seo") {
         const seo = await parser.getSeo();
@@ -57,7 +70,7 @@ export default async function mainMenu(urls: string[]): Promise<void> {
 
       if (section === "images") {
         const images = await parser.getAllImages();
-        const res = await imageMenu(images); // если внутри будет "exit" — выходим глобально
+        const res = await imageMenu(images, menu_choices.images); // если внутри будет "exit" — выходим глобально
         if (res === "exit") return;
       }
     }
